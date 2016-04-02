6fdf97f50c75ae7267ba4e43baf57f580b8cd0fd
elisa-demonki <elisa.demonki@gmail.com>
2016-04-02 14:50:28 +0200

    Create Ein Upload Link in der Adminbar.php

---
+++ Ein Upload Link in der Adminbar.php
@@ -0,0 +1,15 @@
+// Plugin Name: Ein Upload Link in der Adminbar
+
+function bau_ein_upload_link_in_der_adminbar() {
+
+  global $wp_admin_bar;
+  $wp_admin_bar->add_menu( array(
+		'parent'  => 'new-content',
+		'id'      => 'new_media',
+		'title'   => 'Media',
+		'href'    => admin_url( 'media-new.php')
+		'meta'    => false
+	));
+}
+
+add_action( 'wp_before_admin_bar_render', 'bau_ein_upload_link_in_der_adminbar' );
