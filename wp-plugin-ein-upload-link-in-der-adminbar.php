32702fc1c2b4a0eedee67052f580bf57dcbd2103
elisa-demonki <elisa.demonki@gmail.com>
2016-04-02 15:14:20 +0200

    Update wp-plugin-ein-upload-link-in-der-adminbar.php

--- wp-plugin-ein-upload-link-in-der-adminbar.php
+++ wp-plugin-ein-upload-link-in-der-adminbar.php
@@ -1,17 +1,19 @@
-<?php 
+<?php
 
-// Plugin Name: Ein Upload Link in der Adminbar
+// Plugin Name: WP-Schnipsel der Freude - Ein Upload Link in der Adminbar
 
 function bau_ein_upload_link_in_der_adminbar() {
 
   global $wp_admin_bar;
+  
+  if (current_user_can( 'manage_options' ) ) {
+    
   $wp_admin_bar->add_menu( array(
-		'parent'  => 'new-content',
-		'id'      => 'new_media',
-		'title'   => 'Media',
+		'id'      => 'medien-hochladen',
+		'title'   => 'Medien hochladen',
 		'href'    => admin_url( 'media-new.php')
-		'meta'    => false
 	));
+}
 }
 
 add_action( 'wp_before_admin_bar_render', 'bau_ein_upload_link_in_der_adminbar' );
