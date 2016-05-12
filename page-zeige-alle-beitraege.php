bdca6d4f4ebfb1dba591ca83ebf13bbb26512e0b
elisa-demonki <elisa.demonki@gmail.com>
2016-05-12 16:17:31 +0200

    Create page-zeige-alle-beitraege.php

---
+++ page-zeige-alle-beitraege.php
@@ -0,0 +1,42 @@
+<?php
+/*
+Template Name: Zeige alle Beitr&auml;ge
+*/
+?>
+
+<html>
+    <head>
+        <style>
+            
+            body {
+                font-family:sans-serif;
+            }
+            
+            pre {
+                background: black;
+                color: white;
+                padding: 1em;
+            }   
+            
+            code {
+                background-color: black;
+                color: white;
+                padding: 0 0.3em;
+            }
+            
+        </style>
+    </head>
+    <body>
+        
+<?php query_posts('posts_per_page=0'); ?>
+        
+<?php if (have_posts()) : ?><?php while (have_posts()) : the_post(); ?>
+
+<h1><?php the_title(); ?></h1>
+<div><?php the_content(); ?></div>
+
+<?php endwhile; ?>
+<?php endif; ?>
+        
+    </body>
+</html>
