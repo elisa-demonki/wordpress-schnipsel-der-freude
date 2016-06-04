8ee382c24024705a70a433fbf0159dba185eb347
elisa-demonki <elisa.demonki@gmail.com>
2016-06-04 13:55:57 +0200

    Create Shortcode, der die Beitrage anzeigt.php

---
+++ Shortcode, der die Beitrage anzeigt.php
@@ -0,0 +1,16 @@
+<?php
+
+// Plugin Name: Shortcode, der die Beitrage anzeigt
+// Description: Shortcode: [archives]
+// ---------------------------
+// Links: 
+// http://wordpress.stackexchange.com/questions/61674/how-to-make-archive-shortcode
+// https://developer.wordpress.org/reference/functions/wp_get_archives/
+// http://codex.wordpress.org/Shortcode_API
+
+function archive_shortcode( $atts ) {
+ return wp_get_archives('type=postbypost');
+}
+add_shortcode( 'archives', 'archive_shortcode' );
+
+?>
