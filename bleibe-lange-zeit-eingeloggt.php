b3f84085c3feaa9fcd18a00aada3fbd75ff6734a
elisa-demonki <elisa.demonki@gmail.com>
2016-06-04 14:15:32 +0200

    Update bleibe-lange-zeit-eingeloggt.php

--- bleibe-lange-zeit-eingeloggt.php
+++ bleibe-lange-zeit-eingeloggt.php
@@ -1,7 +1,8 @@
 <?php 
 // Plugin Name: Bleibe eingeloggt
 // Description: Wenn WP sich selbstständig ausloggt, gibt es manchmal Probleme mit dem Speichern, wenn man gerade an einem Beitrag oder an einer Seite schreibt.
-// Links: http://www.labnol.org/internet/wordpress-optimization-guide/3931
+// Links: 
+// https://developer.wordpress.org/reference/hooks/auth_cookie_expiration/
 
 add_filter( 'auth_cookie_expiration', 'bleibe_viele_jahre_eingeloggt' );
 function bleibe_viele_jahre_eingeloggt( $expire ) {
