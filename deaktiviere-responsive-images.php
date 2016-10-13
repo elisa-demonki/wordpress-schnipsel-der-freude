40c2b509dc95afaab613a90e091e4afa25f35bf7
elisa-demonki <elisa.demonki@gmail.com>
2016-10-13 13:45:34 +0200

    Update deaktiviere-responsive-images.php

--- deaktiviere-responsive-images.php
+++ deaktiviere-responsive-images.php
@@ -1,7 +1,7 @@
 
 <? php
 
-// Plugin Name: WP-Schnipsel der Freude - Deaktiviere von WP aufgezwungene responsive Images
+// Plugin Name: WP-Schnipsel der Freude - Deaktiviere responsive Images
 
 add_filter( 'wp_calculate_image_srcset_meta', '__return_null' );
 
