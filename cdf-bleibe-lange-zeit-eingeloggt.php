9aa0acef4191e520c124fed506c54868e5332716
elisa-demonki <elisa.demonki@gmail.com>
2016-10-23 18:19:50 +0200

    Update and rename bleibe-lange-zeit-eingeloggt.php to cdf-bleibe-lange-zeit-eingeloggt.php

--- cdf-bleibe-lange-zeit-eingeloggt.php
+++ cdf-bleibe-lange-zeit-eingeloggt.php
@@ -1,10 +1,11 @@
 <?php 
-// Plugin Name: Bleibe eingeloggt
+// Plugin Name: CDF - Bleibe eingeloggt
 // Description: WP loggt dich jetzt nicht mehr automatisch nach 2 Wochen aus.
 // Links: 
 // https://developer.wordpress.org/reference/hooks/auth_cookie_expiration/
 
-add_filter( 'auth_cookie_expiration', 'bleibe_viele_jahre_eingeloggt' );
 function bleibe_viele_jahre_eingeloggt( $expire ) {
   return 3155692600; // 100 Jahre in Sekunden
 }
+
+add_filter( 'auth_cookie_expiration', 'bleibe_viele_jahre_eingeloggt' );
