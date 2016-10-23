cb182d7b37bd6ddda02905fec064aa3e0b56b505
elisa-demonki <elisa.demonki@gmail.com>
2016-10-23 18:20:39 +0200

    Update and rename cdf_woocommerce _customer_ip_address.php to cdf_woocommerce_customer_ip_address.php

--- cdf_woocommerce_customer_ip_address.php
+++ cdf_woocommerce_customer_ip_address.php
@@ -1,12 +1,10 @@
 <?php
-Plugin Name: CDF - Woocommerce _customer_ip_address
-Quelle: toscho - https://gist.github.com/glueckpress/8335883
+// Plugin Name: CDF - Woocommerce _customer_ip_address
+// Quelle: toscho - https://gist.github.com/glueckpress/8335883
 
 function mp1401091554( $null, $id, $key ) {
     if ( '_customer_ip_address' === $key )
         return FALSE;
-
     return $null;
 }
-
 add_filter( 'update_post_metadata', 'mp1401091554', 10, 3 );
